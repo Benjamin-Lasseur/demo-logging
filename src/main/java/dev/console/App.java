0ed4bf339d289b68a0a6292535c0df3d08670137
Benjamin Lasseur <benjamin.lasseur@gmail.com>
2017-10-11 16:00:06 +0200

    Ajout de commentaires

--- src/main/java/dev/console/App.java
+++ src/main/java/dev/console/App.java
@@ -5,12 +5,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class App {
+	/** LOG : Logger */
 	private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
-public static void main(String[] args) {
-LOG.info("Bonjour Slf4J !");
-LOG.info("Implémentation Logback");
-new AppService().executer("valeur 1"); 
+	/**
+	 * Instanciation de l'AppService
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		LOG.info("Bonjour Slf4J !");
+		LOG.info("Implémentation Logback");
+		new AppService().executer("valeur 1");
 
-}
+	}
 }
--- src/main/java/dev/service/AppService.java
+++ src/main/java/dev/service/AppService.java
@@ -4,8 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AppService {
+	/** LOG : Logger */
 	private static final Logger LOG = LoggerFactory.getLogger(AppService.class);
 
+	/**
+	 * Méthode execute
+	 * 
+	 * @param param
+	 */
 	public void executer(String param) {
 		LOG.debug("Traitement 1 : param = {}", param);
 	}
